46aef28a8e9718e957c52d06785817f028c02cc0
Gonewiththewind4 <523443113@qq.com>
2019-03-18 12:17:41 +0000

    Update Bisection.m

--- NumericalSolutonOfNonlinearEquation/Bisection.m
+++ NumericalSolutonOfNonlinearEquation/Bisection.m
@@ -1,4 +1,5 @@
 function x = Bisection(f,a,b,max,eps)
+
 if sign(f(a)) == sign(f(b))
     disp('There is no real solution in this interva;')
     x = [];
@@ -9,6 +10,9 @@ c = b;
 while((f(c)~=0) && (step<max) && (abs(b-a)>=eps))
     
     c = a + (b - a) / 2;
+% Forsythe , Malcolm , and Moler [1977 , p. 162 ] present an example 
+% in which the midpoint computed by (a + b)/2 move s outside of the 
+% interval [a, b] on a machine with limited precision
     
     if(f(c) == 0)
         x = c;
@@ -21,10 +25,13 @@ while((f(c)~=0) && (step<max) && (abs(b-a)>=eps))
         end
     end
     
-step = step + 1;
+    step = step + 1;
 
 end
 
-x = c;
+    x = c;
 
 end
+% using sign(u) != sign(w) rather than wv < 0,
+% since the latter requires an unnecessary multiplication 
+% and could cause an underflow or overflow
